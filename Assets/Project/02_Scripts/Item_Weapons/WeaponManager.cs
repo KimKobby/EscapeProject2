650cb74c6e24f78e0a2d2ab4db27ef17e579f5bf
KimKobby <ihj00478@naver.com>
2024-06-12 20:01:06 +0900

    무기 랜덤 생성, 조합 스크립트 정리
    굳

--- Assets/Project/02_Scripts/Item_Weapons/WeaponManager.cs
+++ Assets/Project/02_Scripts/Item_Weapons/WeaponManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Weapons
 {
-    public class WeaponManager
+    public class WeaponManager : MonoBehaviour
     {
         [SerializeField]
         private List<GameObject> meleeWeapons = new List<GameObject>();
@@ -12,7 +12,33 @@ namespace Weapons
         [SerializeField]
         private List<GameObject> potionWeapons = new List<GameObject>();
 
+        [SerializeField]
+        private GameObject spawnArea;
+        private Vector3 spawnPosition;
 
+        private void Start()
+        {
+            GEN_RandomWeapon();
+        }
+
+        private void GEN_Area()
+        {
+            // 큐브 오브젝트 내부의 랜덤 위치 계산
+            spawnPosition = spawnArea.transform.position + new Vector3(
+                Random.Range(-spawnArea.transform.localScale.x / 2f, spawnArea.transform.localScale.x / 2f),
+                Random.Range(-spawnArea.transform.localScale.y / 2f, spawnArea.transform.localScale.y / 2f),
+                Random.Range(-spawnArea.transform.localScale.z / 2f, spawnArea.transform.localScale.z / 2f)
+            );
+        }
+
+        private void GEN_RandomWeapon()
+        {
+            foreach (GameObject weaponInList in meleeWeapons)
+            {
+                GEN_Area();
+                weaponInList.SetActive(Random.value > 0f);
+                GameObject weapon = Instantiate(weaponInList, spawnPosition, Quaternion.identity);
+            }
+        }
     }
-
 }
--- Assets/Project/02_Scripts/NPC/WeaponCombination.cs
+++ Assets/Project/02_Scripts/NPC/WeaponCombination.cs
@@ -17,33 +17,41 @@ namespace NPC
         public List<GameObject> combinationBox = new List<GameObject>(); // 조합 박스 
         public List<GameObject> objsBox = new List<GameObject>(); // 조합 박스 대체용
 
-        public void WeaponAddList(SelectEnterEventArgs args)  // 소켓 아이템 추가
+
+        // 조합 소켓 아이템 리스트에 추가
+        public void WeaponAddList(SelectEnterEventArgs args) 
         {
             combinationBox.Add(args.interactableObject.transform.gameObject);
             //Debug.Log(args.interactableObject + "_ADD");
         }
 
-        public void WeaponRemoveList(SelectExitEventArgs args)  // 소켓 아이템 제거
+
+        // 조합 소켓 아이템 리스트에 제거
+        public void WeaponRemoveList(SelectExitEventArgs args)
         {
             combinationBox.Remove(args.interactableObject.transform.gameObject);
             //Debug.Log(args.interactableObject + "_Remove");
         }
 
-/*        public void TakeOutWeapon(SelectExitEventArgs args)  // 소켓 아이템 제거
+        /*
+        // 조합 완성 소켓 아이템 제거
+        public void TakeOutWeapon(SelectExitEventArgs args)  
         {
             args.interactableObject.transform.gameObject.GetComponent<Rigidbody>().angularDrag = 0.05f;
             args.interactableObject.transform.gameObject.GetComponent<Rigidbody>().useGravity = true;
             args.interactableObject.transform.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-        }*/
+        }
+        */
 
 
-        public void Combination()  // 무기 조합 기능
+        // 무기 조합 기능
+        public void Combination()  
         {
             OnSocket();
 
             int combinationDamage = 0;  // 조합데미지 0 으로 초기화
 
-            if (combinationBox != null && isCombiantion)
+            if (combinationBox != null && isCombiantion)  // 조합 박스가 빈박스가 아니고 조합 유무가 true 일때 실행
             {
                 isCombiantion = false;
 
@@ -70,7 +78,7 @@ namespace NPC
             }
         }
 
-        
+        // 조합 완성 소켓 칸 오픈 기능
         private void OnSocket()
         {
             Debug.Log(this.transform.GetChild(1).GetChild(4).gameObject.name);
@@ -79,46 +87,46 @@ namespace NPC
             this.transform.GetChild(1).GetChild(4).gameObject.GetComponent<XRSocketInteractor>().interactionLayers = InteractionLayerMask.GetMask("Weapon");
         }
 
-        private void CreateCombinationWeapon(int _combinationDamage)  // 새 조합 무기 생성 기능
+
+        // 새 조합 무기 생성 기능
+        private void CreateCombinationWeapon(int _combinationDamage)  
         {
-            // 첫번째로 들어온 무기를 메인으로 인스턴스
-            GameObject combinationWeapon = Instantiate(combinationBox[0], transform.GetChild(1).GetChild(4).gameObject.transform.position, Quaternion.identity); 
+                // 첫번째로 들어온 무기를 메인으로 인스턴스
+                GameObject combinationWeapon = Instantiate(combinationBox[0], transform.GetChild(1).GetChild(4).gameObject.transform.position, Quaternion.identity);
 
-            // RIgidbody 값 설정
-            combinationWeapon.GetComponent<Rigidbody>().angularDrag = 0.05f;
-            combinationWeapon.GetComponent<Rigidbody>().useGravity = true;
-            combinationWeapon.GetComponent<Rigidbody>().isKinematic = false;
+                // RIgidbody 값 설정
+                combinationWeapon.GetComponent<Rigidbody>().angularDrag = 0.05f;
+                combinationWeapon.GetComponent<Rigidbody>().useGravity = true;
+                combinationWeapon.GetComponent<Rigidbody>().isKinematic = false;
 
-            // 대상의 머티리얼 가져옴
-            Material newMat = combinationWeapon.GetComponent<MeshRenderer>().material; 
+                // 대상의 머티리얼 가져옴
+                Material newMat = combinationWeapon.GetComponent<MeshRenderer>().material;
 
-            // 머티리얼 변경
-            newMat.EnableKeyword("_EMISSION");
-            newMat.SetTexture("_EmissionMap", newMat.GetTexture("_BaseMap"));
-            newMat.SetColor("_EmissionColor", new Color(190f, 8f, 0f));
-            newMat.SetFloat("_EmissionIntensity", 5.5f);
+                // 머티리얼 변경
+                newMat.EnableKeyword("_EMISSION");
+                newMat.SetTexture("_EmissionMap", newMat.GetTexture("_BaseMap"));
+                newMat.SetColor("_EmissionColor", new Color(190f, 8f, 0f));
+                newMat.SetFloat("_EmissionIntensity", 5.5f);
 
-            // 변경된 머티리얼 적용
-            combinationWeapon.GetComponent<MeshRenderer>().material = newMat; 
+                // 변경된 머티리얼 적용
+                combinationWeapon.GetComponent<MeshRenderer>().material = newMat;
 
 
-            if (combinationWeapon != null)
-            {
-                combinationWeapon.GetComponent<Weapon>().stunDamage = _combinationDamage;  // 조합무기의 stunDamage 값을 combinationDamage로 설정
-                //combinationWeapon.GetComponent<Rigidbody>().angularDrag = 0.5f;
-
-                for (int i = 0; i < objsBox.Count; i++)
+                if (combinationWeapon != null)
                 {
-                    objsBox[i].SetActive(false);
+                    combinationWeapon.GetComponent<Weapon>().stunDamage = _combinationDamage;  // 조합무기의 stunDamage 값을 combinationDamage로 설정
+
+                    // 조합 진행 된 무기 제거
+                    for (int i = 0; i < objsBox.Count; i++)
+                    {
+                        objsBox[i].SetActive(false);
+                    }
+                    objsBox.Clear();
                 }
-
-                objsBox.Clear();
-
-            }
-            else
-            {
-                Debug.Log("조합된 무기가 만들어지지 않았습니다.");
-            }
+                else
+                {
+                    Debug.Log("조합 된 무기가 만들어지지 않았습니다.");
+                }
         }
     }
 }
