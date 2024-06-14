7afcf0211f1c5fae84f9964da7203e65e31f810b
jeakeun <worms3302@gmail.com>
2024-06-14 15:30:54 +0900

    충돌확인용

--- Assets/Project/02_Scripts/ControllerInputData.cs
+++ Assets/Project/02_Scripts/ControllerInputData.cs
@@ -1,8 +1,11 @@
 
 using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
+using UnityEngine.UIElements.Experimental;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
@@ -30,6 +33,13 @@ namespace Song
         private float rightJoyStickVal;
         private float inRaySelectVal;
 
+        public GameObject Inventory;
+
+
+        public bool isClicked = false;
+        private bool xButtonClick = false;
+
+
         public float getStickVal()
         {
             return rightJoyStickVal;
@@ -41,13 +51,18 @@ namespace Song
         }
         void Start()
         {
+            Inventory.gameObject.SetActive(false);
             clockCanvas.transform.gameObject.SetActive(false);
+            actionAsset.actionMaps[8].actions[0].canceled += PlayerSitandStand;
         }
 
         void Update()
         {
             LeftHandCheck();
             TriggerClick();
+            InventoryOn();
+            PlayerMoveSpeed();
+
         }
 
         private void TriggerClick()
@@ -58,6 +73,7 @@ namespace Song
             rightJoyStickVal = actionAsset.actionMaps[6].actions[5].ReadValue<Vector2>().y;
 
 
+           
             if (rightControllerVal == 1f)
             {
                 b_RightControllerRay = true;
@@ -101,5 +117,70 @@ namespace Song
                 clockCanvas.transform.gameObject.SetActive(false);
             }
         }
+
+        void InventoryOn()
+        {
+            var leftControllerVal_y = actionAsset.actionMaps[8].actions[1].ReadValue<float>();
+
+            //왼쪽 컨트롤러 y누르면 On
+            if(leftControllerVal_y == 1f)
+            {
+                Inventory.gameObject.SetActive(true);
+            }
+            else
+            {
+                Inventory.gameObject.SetActive(false);
+            }
+        }
+
+
+        void PlayerMoveSpeed()
+        {
+
+            //왼쪽 컨트롤러 중지값
+            var leftControllerVal = actionAsset.actionMaps[2].actions[0].ReadValue<float>();
+
+            //왼쪽 컨트롤러 조이스틱값
+            var leftControllerJoystickval = actionAsset.actionMaps[3].actions[5].ReadValue<Vector2>();
+
+
+            //중지를 눌렀을 때(달리기)
+            if (leftControllerVal == 1f)
+            {
+                //움직임이 없는경우
+                if (leftControllerJoystickval == new Vector2(0, 0))
+                {
+                    
+                   this.GetComponent<ContinuousMoveProviderBase>().moveSpeed = 1f;
+                    
+                }
+                //달리는 중
+                else
+                {
+                    this.GetComponent<ContinuousMoveProviderBase>().moveSpeed = 3f;
+                }
+            }
+            //평소
+            else
+            {
+                this.GetComponent<ContinuousMoveProviderBase>().moveSpeed = 1f;
+            }
+        }
+
+
+        public void PlayerSitandStand(InputAction.CallbackContext action)
+        {
+            xButtonClick = !xButtonClick;
+
+            //X 토글방식 앉기 일어서기
+            if (xButtonClick)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, -0.5f, this.transform.position.z);
+               
+            }
+            else
+                this.transform.position = new Vector3(this.transform.position.x, 0.5f, this.transform.position.z);
+        }
+
     }
 }
