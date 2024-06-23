feb6b62cee0ec1ca7456b4c3f68097643097ae0d
jeakeun <worms3302@gmail.com>
2024-06-23 17:13:17 +0900

    퓨즈박스 머터리얼 변경되는거 귀신나오는거 수정중

--- Assets/3. SJK/02_Scripts/RandomChairImage.cs
+++ Assets/3. SJK/02_Scripts/RandomChairImage.cs
@@ -5,17 +5,19 @@ using System.Collections;
 
 public class RandomChairImage : MonoBehaviour
 {
-    public GameObject[] chairs; // ���� ��ü�� �迭
-    public Image imageToShow; // ��Ÿ�� �̹���
-    private GameObject selectedChair; // ���õ� ����
+    public GameObject[] chairs; // 의자 객체의 배열
+    public Canvas canvasToShow; // 표시할 캔버스
+    public Transform hmdTransform; // HMD의 트랜스폼
+    public Vector3 canvasOffset = new Vector3(0, 0, 1); // HMD로부터의 오프셋
+    private GameObject selectedChair; // 선택된 의자
 
     void Start()
     {
-        // ���� �� �ϳ��� �������� ����
+        // 의자 중 하나를 무작위로 선택
         selectedChair = chairs[Random.Range(0, chairs.Length)];
         Debug.Log("Selected chair: " + selectedChair.name);
 
-        // ���õ� ���ڿ� �̺�Ʈ �ڵ鷯 �߰�
+        // 선택된 의자에 이벤트 리스너 추가
         XRGrabInteractable grabInteractable = selectedChair.GetComponent<XRGrabInteractable>();
         if (grabInteractable != null)
         {
@@ -25,19 +27,28 @@ public class RandomChairImage : MonoBehaviour
 
     void OnChairGrabbed(XRBaseInteractor interactor)
     {
-        StartCoroutine(ShowImage());
+        StartCoroutine(ShowCanvas());
     }
 
-    IEnumerator ShowImage()
+    IEnumerator ShowCanvas()
     {
-        // �̹��� ��Ÿ����
-        imageToShow.gameObject.SetActive(true);
+        // 캔버스를 HMD 앞에 배치
+        PositionCanvasInFrontOfHMD();
 
-        // 2�� ��ٸ���
+        // 캔버스를 표시
+        canvasToShow.gameObject.SetActive(true);
+
+        // 2초 대기
         yield return new WaitForSeconds(2);
 
-        // �̹��� �����
-        imageToShow.gameObject.SetActive(false);
+        // 캔버스를 숨김
+        canvasToShow.gameObject.SetActive(false);
+    }
+
+    private void PositionCanvasInFrontOfHMD()
+    {
+        // HMD 앞에 캔버스 위치 설정
+        canvasToShow.transform.position = hmdTransform.position + hmdTransform.forward * canvasOffset.z + hmdTransform.up * canvasOffset.y + hmdTransform.right * canvasOffset.x;
+        canvasToShow.transform.rotation = Quaternion.LookRotation(canvasToShow.transform.position - hmdTransform.position);
     }
 }
-
